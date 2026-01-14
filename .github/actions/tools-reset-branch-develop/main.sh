c88b9f1ba2ece78cb9abd1cba7498c79c50452e5
FreshGo01 <freshnathawat@gmail.com>
2026-01-14 11:02:52 +0700

    Update main.sh

--- .github/actions/tools-reset-branch-develop/main.sh
+++ .github/actions/tools-reset-branch-develop/main.sh
@@ -261,12 +261,12 @@ show_logs "INFO" "------------------------------------------------"
 
 for repo in "${repo_array[@]}"; do
     # Increment counter
-    ((current_count++))
+    # ((current_count++))
     
     # Pass repo, current_count, and total_repos to the function
-    # process_repo "$repo" "$current_count" "$total_repos"
+    process_repo "$repo" "$current_count" "$total_repos"
     
-    # sleep $SLEEP_DURATION
+    sleep $SLEEP_DURATION
 done
 
 show_logs "INFO" "--- BATCH PROCESS COMPLETE ---"
